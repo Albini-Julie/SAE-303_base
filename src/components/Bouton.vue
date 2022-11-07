cf7d7a89a0fdce150ec635cc65f2579f0458905d
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-07 21:09:51 +0100

    composant bouton

---
+++ src/components/Bouton.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="flex items-center justify-center bg-Rose_foncé  gap-2 rounded-2xl  p-3 text-[20px] md:text-[25px] xl:text-[30px] ">
+        <span class="text-white font-semibold font-noto-sans"><slot/></span>
+    </button>
+</template>
