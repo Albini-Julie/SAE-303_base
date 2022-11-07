e6a787c2d0805f7ac7e300a9c5d026b244959b00
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-07 21:40:35 +0100

    modification router link Accueil

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
+  <div class="mb-20">
   <Header/>
   <div class="flex justify-center">
+  <router-link to="/">
   <h1 class="font-albert-sans text-Rose_foncé text-[50px] md:text-[60px] lg:text-[70px] xl:text-[80px] xl:mt-5 font-bold">Sup'analyse</h1>
+  </router-link>
   </div>
   <div class="flex justify-center">
     <h2 class="font-albert-sans text-center text-Bleu_marine font-semibold h2">L’enseignement supérieur en chiffres</h2>
