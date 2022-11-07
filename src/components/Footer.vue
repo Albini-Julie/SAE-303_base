a312bec442462605ad58a01561aead125cbd04f9
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-07 21:09:57 +0100

    composant footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-Bleu_marine p-5 flex justify-between  items-center">
+    <p class="texte font-bold text-white">Mentions légales</p>
+    <div class="flex gap-2 md:gap-5 lg:gap-10 ">
+        <instagram class="sm:w-[40px] sm:h-[40px] lg:w-[50px] lg:h-[50px]"/>
+        <tiktok class="sm:w-[47px] sm:h-[40px] lg:w-[57px] lg:h-[50px]"/>
+        <youtube class="sm:w-[47px] sm:h-[40px] lg:w-[57px] lg:h-[50px]"/>
+    </div>
+    </div>
+</template>
+
+<script setup>
+import instagram from "../components/icons/Instagram.vue"
+import tiktok from "../components/icons/tiktok.vue"
+import youtube from "../components/icons/youtube.vue"
+
+</script>
