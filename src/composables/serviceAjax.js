898e71dfec6ac6726e725b1bc2f047a69fdcf939
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-08 18:52:51 +0100

    création getEcoles

--- src/composables/serviceAjax.js
+++ src/composables/serviceAjax.js
@@ -20,3 +20,40 @@ export const getEnseign = () => {
         })
         .catch(error => { throw error}) // Cas d'erreur
 }
+
+export const getEcoles = () => {
+    let rq = 'https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/'
+    +'?dataset=fr-esr-principaux-etablissements-enseignement-superieur'
+    +'&q='
+    +'&rows=255'
+    +'&facet=type_d_etablissement'
+    +'&facet=typologie_d_universites_et_assimiles'
+    +'&facet=secteur_d_etablissement'
+    +'&facet=vague_contractuelle'
+    +'&facet=localisation'
+    +'&facet=uai'
+    +'&facet=siret'
+    +'&facet=siren'
+    +'&facet=identifiant_ror'
+    +'&facet=identifiant_grid'
+    +'&facet=identifiant_dataesr'
+    +'&facet=anciens_codes_uai'
+    +'&facet=com_nom'
+    +'&facet=dep_nom'
+    +'&facet=aca_nom'
+    +'&facet=reg_nom'
+    +'&facet=pays_etranger_acheminement'
+    +'&facet=statut_juridique_court'
+    +'&facet=qualification_long'
+    +'&facet=uai_rgp_loi_esr_2013'
+    +'&facet=universites_fusionnees'
+    +'&facet=etablissement_experimental'
+    +'&facet=statut_operateur_lolf'
+    +'&facet=identifiant_programme_lolf_chef_de_file'
+    // Requête concernée
+    return fetch(rq)    // on renvoie la requêteà qui la demande
+        .then(response => {
+            return response.json()  //résultat de la requête en json
+        })
+        .catch(error => { throw error}) // Cas d'erreur
+}
