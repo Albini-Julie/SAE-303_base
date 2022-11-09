efca081f4e0db5f708a60046350fd2bcd0f28672
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-09 10:43:43 +0100

    Ajout des liens RS

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,15 @@
     <div class="bg-Bleu_marine p-5 flex justify-between  items-center">
     <p class="texte font-bold text-white">Mentions légales</p>
     <div class="flex gap-2 md:gap-5 lg:gap-10 ">
-        <instagram class="sm:w-[40px] sm:h-[40px] lg:w-[50px] lg:h-[50px]"/>
-        <tiktok class="sm:w-[47px] sm:h-[40px] lg:w-[57px] lg:h-[50px]"/>
-        <youtube class="sm:w-[47px] sm:h-[40px] lg:w-[57px] lg:h-[50px]"/>
+        <a href="https://www.instagram.com/?hl=fr">
+            <instagram class="sm:w-[40px] sm:h-[40px] lg:w-[50px] lg:h-[50px]"/>
+        </a>
+        <a href="https://www.tiktok.com/fr/">
+            <tiktok class="sm:w-[47px] sm:h-[40px] lg:w-[57px] lg:h-[50px]"/>
+        </a>
+        <a href="https://www.youtube.com/">
+            <youtube class="sm:w-[47px] sm:h-[40px] lg:w-[57px] lg:h-[50px]"/>
+        </a>
     </div>
     </div>
 </template>
